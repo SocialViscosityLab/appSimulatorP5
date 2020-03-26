0eebe333e1576aba85640045c643833595cc568d
Juan Salamanca <juans123456@gmail.com>
2020-03-26 15:59:11 -0500

    DELETED ALL

--- navigator/src/GUI.js
+++
@@ -1,9 +0,0 @@
-/** Manages all GUI elements from the HTML */
-class GUI {
-    static status = document.getElementById("status");
-    static latLon = document.getElementById('map-link');
-    static ghost = document.getElementById('ghost');
-    static camHeight = document.getElementById('camHeight');
-    static mobile = document.getElementById('mobile');
-    static rotation = document.getElementById('rotation');
-}
